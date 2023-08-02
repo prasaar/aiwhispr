79d5e0ae935e0968926ce7f1d410909ac92ad296
prasaar <prasad_arun@yahoo.com>
2023-08-02 08:19:00 +0000

    replace if-else with match-case

--- python/common-functions/initializeContentSite.py
+++ python/common-functions/initializeContentSite.py
@@ -10,6 +10,12 @@ from aiwhisprBaseClasses import vectorDb, siteAuth
 sys.path.append("../common-objects")
 from azureContentSite import azureContentSite
 
+import logging
+
 def initialize(src_type:str,content_site_name:str,src_path:str,src_path_for_results:str,working_directory:str,index_log_directory:str,site_auth:siteAuth,vector_db:vectorDb):
-    if (src_type == 'azureblob'):
-        return azureContentSite(content_site_name=content_site_name,src_path=src_path,src_path_for_results=src_path_for_results,working_directory=working_directory,index_log_directory=index_log_directory,site_auth=site_auth,vector_db=vector_db)
+    logger = logging.getLogger(__name__)
+    
+    match src_type:
+        case 'azureblob':
+            logger.debug('azureContentSite instantiated')
+            return azureContentSite(content_site_name=content_site_name,src_path=src_path,src_path_for_results=src_path_for_results,working_directory=working_directory,index_log_directory=index_log_directory,site_auth=site_auth,vector_db=vector_db)
