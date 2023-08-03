20ce6df29c626a523c0b4f04a39c61c6646f4d7d
prasaar <prasad_arun@yahoo.com>
2023-08-03 06:18:24 +0000

    Added AWS S3 Downloader using boto3 library

---
+++ python/common-objects/awsS3Downloader.py
@@ -0,0 +1,12 @@
+import io
+import os
+import boto3
+from boto3.s3.transfer import TransferConfig
+
+class awsS3Downloader(object):
+
+    def download_s3object_to_file(self, s3_client:boto3.client,s3_bucket_name:str,s3_object_name:str,download_file_name:str ):
+        s3_client.download_file(s3_bucket_name,s3_object_name, downloade_file_name)
+
+
+
