1eaaacf4328f73f5a1418ef56b4f13c21bdc27bc
prasaar <prasad_arun@yahoo.com>
2023-08-05 11:05:05 +0000

    moving this module under common-functions

---
+++ python/common-functions/index_content_site.py
@@ -0,0 +1,66 @@
+import json
+import os
+import glob
+import sys
+import typesense
+import time
+import datetime
+import re
+import getopt
+import configparser
+import io
+import uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, generate_account_sas, ResourceTypes, AccountSasPermissions
+from datetime import datetime, timedelta
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+print(curr_dir)
+os.chdir(curr_dir)
+os.getcwd()
+sys.path.insert(1, os.path.abspath(os.path.join(curr_dir, os.pardir)))
+sys.path.append("../common-functions")
+import index_content_site_for_config
+
+import logging
+logging.basicConfig(level = logging.DEBUG,format = '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
+#logging.basicConfig(level = logging.CRITICAL,format = '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s')
+
+def main(argv):
+   logger = logging.getLogger(__name__)
+   configfile = ''
+   typesense_hostname = ''
+   typesense_portnumber = ''
+   typesense_key = ''
+   site_name = ''
+   src_type = ''
+   src_path = ''
+   src_path_for_results = ''
+   auth_type = ''
+   sas_token = ''
+   site_userid = ''
+   site_password = ''
+
+   opts, args = getopt.getopt(argv,"hC:A:",["configfile=","all="])
+   for opt, arg in opts:
+      if opt == '-h':
+         print('index_content_site.py -C <content_site_config_file>' )
+         print(' Or ' )
+         print('index_content_site.py -A <path_to_directory_with_all_content_site_config_files>')
+         sys.exit()
+      elif opt in ("-C", "--configfile"):
+         configfile = arg
+         logger.info(configfile)
+         index_content_site_for_config.index(configfile)
+      elif opt in ("-A", "--all"):
+         configfiledir = arg
+         globsearchpath = configfiledir + '/*.cfg'
+         myconfigfilelist = [f for f in glob.glob(globsearchpath)]
+         for configfile in myconfigfilelist:
+             print('##Config File : ',configfile)
+             logger.info(configfile)
+             index_content_site_for_config.index(configfile)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
