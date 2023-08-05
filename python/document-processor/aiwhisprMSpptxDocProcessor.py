941dcdd36c9b610a63f7e276bcb98dfa0cfec056
prasaar <prasad_arun@yahoo.com>
2023-08-05 12:36:57 +0000

    Moved the document processor files to a seprate directory

---
+++ python/document-processor/aiwhisprMSpptxDocProcessor.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import io
+from datetime import datetime, timedelta
+import pathlib
+import time
+import textract
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(1, os.path.abspath(os.path.join(curr_dir, os.pardir)))
+sys.path.append("../common-functions")
+import aiwhisprConstants 
+sys.path.append("../base-classes")
+from aiwhisprBaseClasses import srcDocProcessor
+
+import logging
+
+class  getDocProcessor(srcDocProcessor):
+    logger = logging.getLogger(__name__)
+
+    def __init__(self,downloaded_file_path):
+        srcDocProcessor.__init__(self,downloaded_file_path)
+        
+    #public function
+    def extractText(self):
+        #This is a MS Office 2007 onwards .pptx document text extractor
+
+        self.logger.debug('This is a .pptx file form which we will extract text')
+        
+        docx_filepath = self.downloaded_file_path
+        txt_filepath = self.extracted_text_file_path
+
+        text = textract.process(docx_filepath).decode('utf-8')
+        self.logger.debug('Extracted text from pptx file')
+        try:
+            f = open(txt_filepath, "w")
+        except:
+            self.logger.error('Could not not open a file to save the extracted text : %s', txt_filepath)
+        else:
+            f.write(text)
+            f.close()
