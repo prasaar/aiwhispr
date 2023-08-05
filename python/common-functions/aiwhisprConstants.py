50b7ebc5bf0a0ba6c143a76d2a5fbeabb16c42b6
prasaar <prasad_arun@yahoo.com>
2023-08-05 12:38:25 +0000

    Added a dictionary to map file suffix to document processor module name

--- python/common-functions/aiwhisprConstants.py
+++ python/common-functions/aiwhisprConstants.py
@@ -1,11 +1,31 @@
-
-#FILEXTNLIST = ['.txt', '.csv', '.pdf', '.xls', '.xlsx', '.doc', '.docx', '.ppt', '.pptx', '.wiki', '.md', '.py', '.js', '.php' , '.sh', '.c' , '.pl', '.cpp', '.cs', '.h', '.java', '.swift']
-
 #List of of all file extensions[document types] that the app will read, extract text and then encode using LLM
 FILEXTNLIST = ['.txt', '.csv', '.pdf', '.xlsx', '.docx', '.pptx']
 
-#A list of all text file extension types. We will not do an extractText operation on them.
-TXTFILEXTN = ['.txt', '.csv', '.py', '.js', '.php' , '.sh', '.c' , '.pl', '.cpp', '.cs', '.h', '.java', '.swift']
-
 TXTCHUNKSIZE = 700
 S3MAXKEYS=1000   #MAXIMUM ALLOWED VALUE IS 1000
+
+# This is a dictionary that maps the file suffix to the moduleName that can process the document
+# Edit this when you add a module to handle a new document type (.suffix)
+# The python module is loaded dynamically
+DOCPROCESSORMODULES={
+    '.txt': 'aiwhisprTextDocProcessor',
+    '.csv': 'aiwhisprTextDocProcessor',
+    '.py': 'aiwhisprTextDocProcessor',
+    '.js': 'aiwhisprTextDocProcessor',
+    '.php': 'aiwhisprTextDocProcessor',
+    '.sh': 'aiwhisprTextDocProcessor',
+    '.c': 'aiwhisprTextDocProcessor',
+    '.pl':'aiwhisprTextDocProcessor',
+    '.cpp':'aiwhisprTextDocProcessor',
+    '.cs':'aiwhisprTextDocProcessor',
+    '.h':'aiwhisprTextDocProcessor',
+    '.java':'aiwhisprTextDocProcessor',
+    '.swift':'aiwhisprTextDocProcessor',
+    '.pdf':'aiwhisprPdfDocProcessor',
+    '.docx':'aiwhisprMSdocxDocProcessor',
+    '.xlsx':'aiwhisprMSxlsxDocProcessor',
+    '.pptx':'aiwhisprMSpptxDocProcessor'
+}
+
+#A list of all text file extension types. We will not do an extractText operation on them.
+#TXTFILEXTN = ['.txt', '.csv', '.py', '.js', '.php' , '.sh', '.c' , '.pl', '.cpp', '.cs', '.h', '.java', '.swift']
