f1eb71f2a17b02c6cebb755ecd1207b0f5045468
prasaar <prasad_arun@yahoo.com>
2023-08-06 11:11:39 +0000

    removed unnecessary module import

--- python/common-objects/awsS3Downloader.py
+++ python/common-objects/awsS3Downloader.py
@@ -5,8 +5,5 @@ from boto3.s3.transfer import TransferConfig
 
 class awsS3Downloader(object):
 
-    def download_s3object_to_file(self, s3_client:boto3.client,s3_bucket_name:str,s3_object_name:str,download_file_name:str ):
-        s3_client.download_file(s3_bucket_name,s3_object_name, download_file_name)
-
-
-
+    def download_s3object_to_file(self, s3_client:boto3.client,s3_bucket_name:str,s3_object_name:str,download_file_path:str ):
+        s3_client.download_file(s3_bucket_name,s3_object_name, download_file_path)
