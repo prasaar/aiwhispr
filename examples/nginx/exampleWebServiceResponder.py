3b4a06c2f7e82bcea445e675c1f76a3c75793109
prasaar <prasad_arun@yahoo.com>
2023-08-11 08:24:23 +0000

    Changed to handle textSearch results and show side-by-side for comparison with semantic search results

--- examples/nginx/exampleWebServiceResponder.py
+++ examples/nginx/exampleWebServiceResponder.py
@@ -12,7 +12,7 @@ import numpy as np
 from numpy import sin, cos, pi, linspace
 import time
 import urllib.parse
-
+from bs4 import BeautifulSoup
 
 search_service_url = 'http://127.0.0.1:5002/aiwhispr'
 top_level_domain='ZZZ.ZZZZ.COM'
@@ -32,7 +32,7 @@ def get_html_header():
 
 def get_html_style():
 #    return '<style>.column{float:left;width:calc(25% - 1rem);margin-top:1rem;margin-left:1 rem;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;border:1px solid black;padding:10px;margin:5px;border-radius:10px} .row:after{content:"";display:table;clear:both;} @media screen and (max-width:1000px){ .column{ width:100%;display:block;margin-bottom:20px;}} </style>'
-    return '<style>.body{background-color:#eee} .column{float:left;width:calc(25% - 1rem);margin-top:1rem;margin-left:1 rem;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;border:0px solid black;padding:10px;margin:5px;border-radius:10px;}  @media screen and (max-width:1000px){ .column{ width:100%;display:block;margin-bottom:20px;}} .linktopost{font-size:0.8rem;word-wrap:break-word} .numberofanswers{text-align:center;font-size:1.0rem;border-width:1px;border-radius:10px;border-style:solid;border-color:solid black;border-padding:1em;background-image: linear-gradient(284deg, #fedd4e, #fcb43a)} .posttitle{font-size:1.1rem;font-weight:bold;margin-top:10px} .question{word-wrap:break-word} .postcard{box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;border:1px solid black;padding:2px;margin:2px;border-radius:10px;font-size:16px;background-color:#FFFFFF} </style>'
+    return '<style>.body{background-color:#eee} .column{float:left;width:calc(49% - 1rem);margin-top:1rem;margin-left:1 rem;box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;border:0px solid black;padding:10px;margin:5px;border-radius:10px;}  @media screen and (max-width:1000px){ .column{ width:100%;display:block;margin-bottom:20px;}} .linktopost{font-size:0.8rem;word-wrap:break-word} .numberofanswers{text-align:center;font-size:1.0rem;border-width:1px;border-radius:10px;border-style:solid;border-color:solid black;border-padding:1em;background-image: linear-gradient(284deg, #fedd4e, #fcb43a)} .posttitle{font-size:1.1rem;font-weight:bold;margin-top:10px} .question{word-wrap:break-word} .postcard{box-sizing:border-box;-moz-box-sizing:border-box;-webkit-box-sizing:border-box;border:1px solid black;padding:2px;margin:2px;border-radius:10px;font-size:16px;background-color:#FFFFFF} </style>'
 
 ###Remeber to change the log file path before running this in production
 logging.basicConfig(filename='exampleWebServiceResponder.log', level=logging.DEBUG)
@@ -51,9 +51,21 @@ def semantic_search():
    html_output = html_output + get_html_style() 
    html_output = html_output + '<div class="row">'
 
-   query_url=search_service_url + '?query='+ urllib.parse.quote(input_query) + '&resultformat=html&withtextsearch=N'
+   query_url=search_service_url + '?query='+ urllib.parse.quote(input_query) + '&resultformat=html&withtextsearch=Y'
    r = requests.get(query_url)
-   html_output = html_output + r.text + '</div>' + end_html_body() + '</html>'
+ 
+   soup = BeautifulSoup(r.text, 'lxml')
+   semantic_match_results = str(soup.find_all("div", class_="aiwhisprSemanticSearchResults")[0])
+   app.logger.debug("Found semantic search results: %s ", semantic_match_results)
+   text_match_results = str(soup.find_all("div", class_="aiwhisprTextSearchResults")[0])
+   app.logger.debug("Found text search results: %s ", text_match_results)
+
+   html_output = html_output + ' <div class="column">' + '<p><b>Semantic Search Results</b></p><br>' + str(semantic_match_results) + '</div>'
+   html_output = html_output + ' <div class="column">' + '<p><b>Text Search Results</b></p><br>' + str(text_match_results) + '</div>'   
+   html_output = html_output + '</div>'  # End of div row
+   html_output = html_output + end_html_body() + '</html>'
+
+   #html_output = html_output + r.text + '</div>' + end_html_body() + '</html>'
 
    return html_output
 
