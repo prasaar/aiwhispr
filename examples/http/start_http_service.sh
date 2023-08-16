00cd3f773cfd7af98010c159779c29c8667392ec
Arun Prasad <arun@aiwhispr.com>
2023-08-16 13:42:22 +1000

    Print the process id

--- examples/http/start_http_service.sh
+++ examples/http/start_http_service.sh
@@ -25,18 +25,24 @@ case "${unameOut}" in
 esac
 echo ${machine}
 
+if [ "${machine}" = "Mac" ]
+then
+DYLD_LIBRARY_PATH=/opt/homebrew/lib
+export DYLD_LIBRARY_PATH
+fi
+
 echo "Start the search service"
 case "${machine}" in
-    Mac) (nohup $AIWHISPR_HOME/shell/start-search-service.sh -H 127.0.0.1 -P 5002 -C $AIWHISPR_HOME/config/content-site/sites-available/example_bbc.filepath.macos.cfg &> /tmp/example_bbc.searchservice.log &);;
-    *) (nohup $AIWHISPR_HOME/shell/start-search-service.sh -H 127.0.0.1 -P 5002 -C $AIWHISPR_HOME/config/content-site/sites-available/example_bbc.filepath.cfg &> /tmp/example_bbc.searchservice.log &);;
+    Mac) (nohup $AIWHISPR_HOME/shell/start-search-service.sh -H 127.0.0.1 -P 5002 -C $AIWHISPR_HOME/config/content-site/sites-available/example_bbc.filepath.macos.cfg &> /tmp/example_bbc.searchservice.log &; echo $!);;
+    *) (nohup $AIWHISPR_HOME/shell/start-search-service.sh -H 127.0.0.1 -P 5002 -C $AIWHISPR_HOME/config/content-site/sites-available/example_bbc.filepath.cfg &> /tmp/example_bbc.searchservice.log &;echo $!);;
 esac
 
 echo
 echo "Start the exampleHttpResponder"
 cd $AIWHISPR_HOME/examples/http
-(python3 exampleHttpResponder.py &> /tmp/example_bbc.exampleHttpResponder.log &);
+(python3 exampleHttpResponder.py &> /tmp/example_bbc.exampleHttpResponder.log &; echo $!);
 
 echo
 echo "Start a python HttpServer at port 9000"
 cd $AIWHISPR_HOME/examples/http
-(python3 -m http.server 9000 &> /tmp/example_bbc.httpServer.log &);
+(python3 -m http.server 9000 &> /tmp/example_bbc.httpServer.log &; echo $!);
