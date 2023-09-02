06363b14f093cd31d331549b2795dcdf386fbda0
prasaar <arun@aiwhispr.com>
2023-09-02 03:15:44 +0000

    fixed python -> python3 for spacy install

--- shell/install_python_packages.sh
+++ shell/install_python_packages.sh
@@ -7,8 +7,8 @@ echo "pip3 install -U pip setuptools wheel"
 pip3 install -U pip setuptools wheel
 echo "pip3 install -U spacy"
 pip3 install -U spacy
-echo "python -m spacy download en_core_web_sm"
-python -m spacy download en_core_web_sm
+echo "python3 -m spacy download en_core_web_sm"
+python3 -m spacy download en_core_web_sm
 echo "pip3 install spacy-language-detection"
 pip3 install spacy-language-detection
 echo "pip3 install -U sentence-transformers"
